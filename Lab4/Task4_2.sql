9e5f6cb872fe93bd3c03ff5554594d66aed10f78
Boris Zinchenko <Digstiks@gmail.com>
2024-05-22 15:42:22 +0300

    Исправлены ошибки

--- Lab4/Task4_2.sql
+++ Lab4/Task4_2.sql
@@ -2,11 +2,9 @@
 --("HumanResources"."Employee").  Примечание   к   ранжированию  –
 --ранг  может  начинаться  с  произвольного  числа   (например,  2  или  5),
 --главное – ранг не должен уменьшаться.
-SELECT 
-    emp."BusinessEntityID",
-    emp."JobTitle",
-    ROW_NUMBER() OVER (ORDER BY emp."JobTitle") + 1 AS "Rank"
-FROM 
-    "HumanResources"."Employee" emp
-ORDER BY 
-    "Rank";
+SELECT "BusinessEntityID", "JobTitle",
+       (SELECT COUNT(*)
+        FROM "HumanResources"."Employee" e2
+        WHERE e2."JobTitle" <= e1."JobTitle") + 4 AS "Rank"
+FROM "HumanResources"."Employee" e1
+ORDER BY "Rank";
--- Lab4/Task4_5.sql
+++ Lab4/Task4_5.sql
@@ -1,18 +1,12 @@
 --Показать товары, цена которых меньше средней цены в том же стиле
 --(Таблица  Production.Product).  Показать поля  Name,  ListPrice и Style.
-SELECT 
-    p."Name",
-    p."ListPrice",
-    p."Style"
-FROM 
-    "Production"."Product" p
-WHERE 
-    p."ListPrice" < 
-    (
-        SELECT 
-            AVG(p2."ListPrice")
-        FROM 
-            "Production"."Product" p2
-        WHERE 
-            p2."Style" = p."Style"
-    );
+WITH "AveragePrices" AS 
+    (SELECT "Style", AVG("ListPrice") AS "AverageListPrice"
+     FROM "Production"."Product"
+     GROUP BY "Style")
+SELECT p."Name", p."ListPrice" , p."Style"
+FROM "Production"."Product" p
+JOIN "AveragePrices" ap
+ON p."Style" = ap."Style"
+WHERE p."ListPrice" < ap."AverageListPrice"
+  AND p."ListPrice" > 0;
